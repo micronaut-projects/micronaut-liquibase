b2c25e81a480f0559a18f256f2b884d60c7962af
Milenko Supic <supic82@gmail.com>
2023-06-16 10:27:41 +0200

    Refactored native tests to not use micronaut-platform

--- test-suite-graal-h2/build.gradle.kts
+++ test-suite-graal-h2/build.gradle.kts
@@ -1,23 +1,7 @@
 plugins {
-    id("io.micronaut.application")
-    id("io.micronaut.build.internal.liquibase-native-tests")
     id("io.micronaut.build.internal.liquibase.test-suite-graal")
 }
 
 dependencies {
-    runtimeOnly("com.h2database:h2")
-}
-
-application {
-    mainClass.set("micronaut.example.Application")
-}
-
-micronaut {
-    version(libs.versions.micronaut.platform.get())
-    runtime("netty")
-    testRuntime("junit5")
-    processing {
-        incremental(false)
-        annotations("micronaut.example.*")
-    }
+    runtimeOnly(mnSql.h2)
 }
--- test-suite-graal-mariadb/build.gradle.kts
+++ test-suite-graal-mariadb/build.gradle.kts
@@ -1,25 +1,7 @@
 plugins {
-    id("io.micronaut.application")
-    id("io.micronaut.test-resources")
-    id("io.micronaut.build.internal.liquibase-native-tests")
     id("io.micronaut.build.internal.liquibase.test-suite-graal")
 }
 
 dependencies {
-    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    runtimeOnly(mnSql.mariadb.java.client)
 }
-
-application {
-    mainClass.set("micronaut.example.Application")
-}
-
-micronaut {
-    version(libs.versions.micronaut.platform.get())
-    runtime("netty")
-    testRuntime("junit5")
-    processing {
-        incremental(false)
-        annotations("micronaut.example.*")
-    }
-}
-
