932705926e9192bafa221504aa25fa428b1abe8c
Sergio del Amo Caballero <sergio.delamo@softamo.com>
2024-08-19 11:54:03 +0200

    disable tests without docker available

--- test-suite-graal-mariadb/build.gradle.kts
+++ test-suite-graal-mariadb/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     id("io.micronaut.build.internal.liquibase.test-suite-graal")
 }
-
+dependencies {
+    testImplementation(platform(mnTestResources.boms.testcontainers))
+    testImplementation(libs.testcontainers.junit.jupiter)
+}
 dependencies {
     runtimeOnly(mnSql.mariadb.java.client)
     testImplementation(mnTestResources.micronaut.test.resources.extensions.junit.platform) {
