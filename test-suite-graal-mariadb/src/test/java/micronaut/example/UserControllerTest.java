69557668d38bbb54bf54b5c8591b9e7f12b750c2
Sergio del Amo <sergio.delamo@softamo.com>
2025-12-29 15:11:23 +0100

    remove test-resources dependency (#731)
    * remove test-resources dependency
    
    * disabled in native image

--- test-suite-graal-mariadb/src/test/java/micronaut/example/UserControllerTest.java
+++ test-suite-graal-mariadb/src/test/java/micronaut/example/UserControllerTest.java
@@ -1,22 +1,32 @@
 package micronaut.example;
 
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.client.BlockingHttpClient;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
-import io.micronaut.test.extensions.junit5.annotation.TestResourcesScope;
+import io.micronaut.test.support.TestPropertyProvider;
 import jakarta.inject.Inject;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.condition.DisabledInNativeImage;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.Map;
+
+@DisabledInNativeImage
 @MicronautTest
-@TestResourcesScope("mariadb-scope")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Testcontainers(disabledWithoutDocker = true)
-class UserControllerTest {
+class UserControllerTest implements TestPropertyProvider {
+    @Override
+    public @NonNull Map<String, String> getProperties() {
+        return MariaDb.getProperties();
+    }
 
     @Inject
     @Client("/")
