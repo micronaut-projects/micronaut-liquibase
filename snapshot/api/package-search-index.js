2494e610de571ae151a8fc668b2e92327871ce5b
sdelamo <sdelamo@users.noreply.github.com>
2025-12-29 14:15:41 +0000

    Deploying to gh-pages - 14:15:41

--- snapshot/api/package-search-index.js
+++ snapshot/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.liquibase"},{"l":"io.micronaut.liquibase.endpoint"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.micronaut.liquibase"},{"l":"io.micronaut.liquibase.endpoint"},{"l":"io.micronaut.liquibase.info"}];updateSearchResults();
