cee0a6eee8638384a9d0024f83908f91688b6561
radovanradic <radovanradic@users.noreply.github.com>
2026-05-04 11:47:26 +0000

    Deploying to gh-pages - 11:47:26

--- snapshot/api/package-search-index.js
+++ snapshot/api/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"io.micronaut.liquibase"},{"l":"io.micronaut.liquibase.endpoint"},{"l":"io.micronaut.liquibase.info"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"io.micronaut.liquibase"},{"l":"io.micronaut.liquibase.endpoint"}];updateSearchResults();
